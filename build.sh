371da20d9cf9294047f64a972f6fb065a28e0611
James Addison <james@reciperadar.com>
2019-10-09 10:19:04 -0700

    Initial build scripts

---
+++ build.sh
@@ -0,0 +1,20 @@
+IMAGE_NAME='registry.gitlab.com/openculinary/ingredient-phrase-tagger'
+IMAGE_COMMIT=$(git rev-parse --short HEAD)
+
+if [ -n "${GITLAB_USER_ID}" ]; then
+    # Override the default 'overlay' storage driver, which fails GitLab builds
+    export STORAGE_DRIVER='vfs'
+
+    # Workaround from https://major.io for 'overlay.mountopt' option conflict
+    sed -i '/^mountopt =.*/d' /etc/containers/storage.conf
+fi
+
+container=$(buildah from docker.io/mtlynch/ingredient-phrase-tagger:latest)
+buildah run ${container} -- mkdir model
+buildah run ${container} -- ./bin/train-prod-model model
+buildah commit --squash --rm ${container} ${IMAGE_NAME}:${IMAGE_COMMIT}
+
+if [ -n "${GITLAB_USER_ID}" ]; then
+    REGISTRY_AUTH_FILE=${HOME}/auth.json echo "${CI_REGISTRY_PASSWORD}" | buildah login -u "${CI_REGISTRY_USER}" --password-stdin ${CI_REGISTRY}
+    buildah push ${IMAGE_NAME}:${IMAGE_COMMIT}
+fi
